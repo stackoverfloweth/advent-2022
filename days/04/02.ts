6c4045c284250106dfc17f41b1160d563f20f9a9
Evan Sutherland <stackoverfloweth@gmail.com>
2022-12-04 12:11:10 -0600

    day 4

---
+++ days/04/02.ts
@@ -0,0 +1,35 @@
+import { input } from './input'
+
+const elfPairs = input.split('\n').map(pair => pair.split(',')) as [AssignmentString, AssignmentString][]
+
+type AssignmentString = `${number}-${number}`
+type Assignment = [number, number]
+
+function getSections(input: AssignmentString): Assignment {
+  const [first, second] = input.split('-')
+
+  return [parseInt(first), parseInt(second)]
+}
+
+function checkOverlap(pair: [AssignmentString, AssignmentString]): boolean {
+  const [first, second] = pair.map(getSections)
+
+  return hasOverlap(first, second)
+}
+
+function hasOverlap(child: Assignment, parent: Assignment): boolean {
+  const [childStart, childEnd] = child
+  const [parentStart, parentEnd] = parent
+
+  return childEnd >= parentStart && childStart <= parentEnd
+}
+
+const overlapCount = elfPairs.reduce((sum, pair) => {
+  if (checkOverlap(pair)) {
+    sum += 1
+  }
+
+  return sum
+}, 0)
+
+console.log(overlapCount)
